79914976f408c6895b3ec67aff40ce4ef0ca4621
kikuchishiho <kikuchishiho@10.95.2.180>
2025-05-20 13:53:44 +0900

    あああ

---
+++ src/com/library/BookExercise.java
@@ -0,0 +1,18 @@
+package com.library;
+
+public class BookExercise {
+    public static void main(String[] args) {
+        // ここで Book クラスのインスタンスを作成し、情報を表示してください
+    	Book myBook = new Book("吾輩は猫である","夏目漱石");
+    	myBook.displayInfo();
+    }
+}
+
+class Book {
+    String title;
+    String author;
+    
+    // コンストラクタをここに実装
+    public Book
+    // 本の情報を表示するメソッドをここに実装
+}
